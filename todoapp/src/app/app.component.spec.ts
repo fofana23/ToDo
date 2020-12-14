baa39d56df88302efac3803f5f8340ddc5144051
fofana23 <alfof1997@gmail.com>
2020-12-13 21:01:48 -0600

    noteupdate

--- todoapp/src/app/app.component.spec.ts
+++ todoapp/src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'todoapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todoapp');
   });
 
   it('should render title', () => {
--- todoapp/src/app/app.component.ts
+++ todoapp/src/app/app.component.ts
@@ -9,8 +9,8 @@ import { NoteService } from './services/note.service';
 })
 export class AppComponent implements OnInit {
 
-  note: Note={} as Note;
-  newNote: Note={} as Note;
+  note: Note = {} as Note;
+  newNote: Note = {} as Note;
   notes: Array<Note> = [];
 
   constructor(private service: NoteService) { }
@@ -26,22 +26,40 @@ export class AppComponent implements OnInit {
     );
   }
 
-  // addAnimal() {
-  //   this.service.addAnimal(this.newAnimal).subscribe(
-  //     data => console.dir(data),
-  //     error => console.log(error),
-  //     () => this.loadData()
-  //   );
-  // }
+  addNote() {
+    let note = {} as Note;
+    note.title = "Template";
+    this.service.addNote(note).subscribe(
+      data => this.newNote = data,
+      error => console.log(error),
+      () => this.loadData()
+    );
+  }
 
 
-  // updateAnimal() {
-  //   this.service.updateAnimal(this.newAnimal).subscribe(
-  //     data => console.dir(data),
-  //     error => console.log(error),
-  //     () => this.loadData()
-  //   );
-  // }
+  updateNote() {
+    this.service.updateNote(this.newNote).subscribe(
+      data => console.dir(data),
+      error => console.log(error),
+      () => this.loadData()
+    );
+  }
 
+  editNote(note: Note){
+    this.newNote = note;
+  }
+
+  deleteNote(id: number){
+    this.service.deleteNote(id).subscribe(
+      data => console.log(data),
+      error => console.log(error),
+      () => this.loadData()
+    );
+  }
+
+  reset() {
+    this.newNote = {} as Note;
+
+  }
 
 }
